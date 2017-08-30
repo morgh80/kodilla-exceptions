5d1def352255ab4967452e4b268cf484dfdd8f0e
Aeronaut <aeronaut@MacBook-Pro-Aeronaut-2.local>
2017-08-30 20:15:30 +0200

    Excersise 8.4 updated. To be reviewed

--- src/main/java/com/kodilla/exception/challenge84/FlightSearch.java
+++ src/main/java/com/kodilla/exception/challenge84/FlightSearch.java
@@ -1,6 +1,7 @@
 package com.kodilla.exception.challenge84;
 
 import java.util.Map;
+import java.util.stream.Stream;
 
 public class FlightSearch {
 
@@ -10,6 +11,9 @@ public class FlightSearch {
         this.availableAirports = availableAirports;
     }
 
+// Wersja z loop for
+
+ /*
     public Boolean findFlight(Flight flight) throws RouteNotFoundException {
         Boolean hasRoute = false;
         Integer isInMapCounter = 0;
@@ -35,6 +39,36 @@ public class FlightSearch {
         }
 
         return hasRoute;
+    }
+    */
+
+
+    public Boolean findFlight(Flight flight) throws RouteNotFoundException {
+        final Boolean[] hasRoute = {false};
+        final Integer[] isInMapCounter = {0}; // Na początku wywalał mi bląd, że zmienne w streamie powinny być final. Użyłem rozwiązania zasugeroanego przez Intellij, ale nie do końca je łąpię dlacze
+        availableAirports.entrySet().stream()
+                .forEach((Map.Entry<String, Boolean> entry) -> {
+                            if (entry.getKey().equals(flight.getArrivalAirport())) {
+                                if (entry.getValue()) {
+                                    hasRoute[0] = true;
+                                    System.out.println("Route found");
+                                } else {
+//                                    throw new RouteNotFoundException("Arrival airport is unavailable");
+                                    System.out.println("Arrival airport is unavailable"); // po zmianie for na streama, nie Intellij nie pozwala mi rzucić wyjątkiem, nie rozumiem dlaczego. Tymczasowo zmieniłem na sout
+                                }
+                            }
+
+                            if (!entry.getKey().equals(flight.getArrivalAirport())) {
+                                isInMapCounter[0]++;
+                            }
+                        }
+                );
+
+        if (isInMapCounter[0] == availableAirports.size()) {
+            throw new RouteNotFoundException("Arrival airport not found in airports list");
+        }
+
+        return hasRoute[0];
     }
 
 }
